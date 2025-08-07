6ac6a0cea9600ef8224cdf9fa72260177880abdf
nilslambertz <nilslambertz@web.de>
2025-08-08 01:38:05 +0200

    WIP pattern matching

--- lang/expression-parser/src/main/java/tools/vitruv/neojoin/expression_parser/PatternMatcher.java
+++ lang/expression-parser/src/main/java/tools/vitruv/neojoin/expression_parser/PatternMatcher.java
@@ -1,24 +1,19 @@
 package tools.vitruv.neojoin.expression_parser;
 
-import jakarta.inject.Inject;
+import com.google.inject.Injector;
 import org.apache.log4j.Logger;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.eclipse.viatra.query.runtime.api.ViatraQueryEngine;
-import org.eclipse.viatra.query.runtime.api.ViatraQueryEngineOptions;
 import org.eclipse.viatra.query.runtime.emf.EMFScope;
-import org.eclipse.viatra.query.runtime.matchers.backend.IQueryBackendFactory;
-import org.eclipse.viatra.query.runtime.rete.matcher.ReteBackendFactory;
-import org.eclipse.xtext.common.types.TypesPackage;
 import org.eclipse.xtext.resource.XtextResourceSet;
 import org.eclipse.xtext.xbase.XExpression;
-import org.eclipse.xtext.xbase.XbasePackage;
+import org.eclipse.xtext.xbase.XbaseStandaloneSetup;
 import tools.vitruv.neojoin.aqr.AQR;
 import tools.vitruv.neojoin.aqr.AQRFeature;
 import viatra.SkipIntermediateReference;
 import viatra.SkipIntermediateReferencePattern;
-import viatra.SkipIntermediateReferenceWithReturn;
-import viatra.SkipIntermediateReferenceWithReturnPattern;
 
 import java.util.List;
 
@@ -26,56 +21,30 @@ public class PatternMatcher {
 	private static final Logger log = Logger.getLogger(PatternMatcher.class);
 	private final AQR aqr;
 
-	@Inject
-	XtextResourceSet resourceSet;
-
-
 	public PatternMatcher(final AQR aqr) {
 		this.aqr = aqr;
 	}
 
 	public void matchAndExtract() {
 		log.info("Pattern matching called");
+		log.info("AQR: " + aqr);
 
 		final List<AQRFeature> features = aqr.classes().stream().flatMap(targetClass -> targetClass.features().stream()).toList();
 		final List<AQRFeature.Reference> referenceFeatures = features.stream().filter(feature -> feature instanceof AQRFeature.Reference).map(feature -> (AQRFeature.Reference) feature).toList();
 
 		final AQRFeature.Reference reference = referenceFeatures.getFirst();
 		final XExpression expression = reference.kind().expression();
+		log.info("expression: " + expression);
 
-		try {
-			SkipIntermediateReferencePattern instance = SkipIntermediateReferencePattern.instance();
-			SkipIntermediateReferencePattern.Match match = instance.instantiate().newMatch(expression);
-			log.info(String.format("SkipIntermediateReferencePattern matched: %s", match));
-		} catch (Exception e) {
-			log.error("Exception during Pattern matching", e);
-		}
-
-		try {
-			SkipIntermediateReferenceWithReturnPattern instance = SkipIntermediateReferenceWithReturnPattern.instance();
-			SkipIntermediateReferenceWithReturnPattern.Match match = instance.instantiate().newMatch(expression, null, null, null);
-			log.info(String.format("SkipIntermediateReferenceWithReturnPattern matched: %s", match));
-			log.info(String.format("SkipIntermediateReferenceWithReturnPattern sourceTarget: %s", match.getSourceTarget()));
-			log.info(String.format("SkipIntermediateReferenceWithReturnPattern flatMapTarget: %s", match.getFlatMapTarget()));
-			log.info(String.format("SkipIntermediateReferenceWithReturnPattern intermediateReferenceTarget: %s", match.getIntermediateReferenceTarget()));
-		} catch (Exception e) {
-			log.error("Exception during Pattern matching", e);
-		}
-
-
-		IQueryBackendFactory reteFactory = ReteBackendFactory.INSTANCE;
-		ViatraQueryEngineOptions options = new ViatraQueryEngineOptions.Builder().withDefaultBackend(reteFactory).build();
-		ResourceSet resourceSet = new ResourceSetImpl();
-		resourceSet.getPackageRegistry().put(XbasePackage.eNS_URI, XbasePackage.eINSTANCE);
-		resourceSet.getPackageRegistry().put(TypesPackage.eNS_URI, TypesPackage.eINSTANCE);
-
-		ViatraQueryEngine engine = ViatraQueryEngine.on(new EMFScope(resourceSet), options);
-		SkipIntermediateReferencePattern.Matcher skipIntermediateReferencePattern = SkipIntermediateReference.instance().getSkipIntermediateReferencePattern(engine);
-		final var engineTest = skipIntermediateReferencePattern.getOneArbitraryMatch(expression).orElse(null);
-		log.info(String.format("engineTest matched: %s", engineTest));
-
-		SkipIntermediateReferenceWithReturnPattern.Matcher skipIntermediateReferenceWithReturnPattern = SkipIntermediateReferenceWithReturn.instance().getSkipIntermediateReferenceWithReturnPattern(engine);
-		final var engineTest2 = skipIntermediateReferenceWithReturnPattern.getAllMatches(expression, null, null, null);
-		log.info(String.format("engineTest2 matched: %s", engineTest2));
+		Injector injector = new XbaseStandaloneSetup().createInjectorAndDoEMFRegistration();
+		XtextResourceSet resourceSet = injector.getInstance(XtextResourceSet.class);
+		resourceSet.getResourceFactoryRegistry().getProtocolToFactoryMap().put("dummy", new XMIResourceFactoryImpl());
+		Resource exprRes = resourceSet.createResource(URI.createURI("dummy:/expr.xbase"));
+		exprRes.getContents().add(expression);
+		EMFScope scope = new EMFScope(resourceSet);
+		ViatraQueryEngine engine = ViatraQueryEngine.on(scope);
+		SkipIntermediateReference.instance().prepare(engine);
+		final var matcher = SkipIntermediateReferencePattern.Matcher.on(engine);
+		log.info(String.format("SkipIntermediateReferencePattern matched: %s", matcher.getAllMatches()));
 	}
 }
