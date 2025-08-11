584e16dffeb19ef3411b1babc21d590ff4e971c0
nilslambertz <nilslambertz@web.de>
2025-08-11 23:24:19 +0200

    WIP ViewExtractor

--- lang/backend-tgg/operators/src/main/java/tools/vitruv/optggs/operators/ViewExtractor.java
+++ lang/backend-tgg/operators/src/main/java/tools/vitruv/optggs/operators/ViewExtractor.java
@@ -1,11 +1,16 @@
 package tools.vitruv.optggs.operators;
 
-import tools.vitruv.neojoin.aqr.*;
+import tools.vitruv.neojoin.aqr.AQR;
+import tools.vitruv.neojoin.aqr.AQRFeature;
+import tools.vitruv.neojoin.aqr.AQRFrom;
+import tools.vitruv.neojoin.aqr.AQRJoin;
+import tools.vitruv.neojoin.aqr.AQRTargetClass;
 import tools.vitruv.optggs.operators.selection.Pattern;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public class ViewExtractor {
     public static View viewFromAQR(AQR aqr) {
@@ -20,7 +25,6 @@ public class ViewExtractor {
 
     private static Query queryFromTargetClass(AQRTargetClass targetClass) {
         final FQN source = fqn(targetClass.source().from());
-        // TODO: How to get target fqn??
         final FQN target = fqn(targetClass.name());
 
         final Map<String, FQN> namedRefs = new HashMap<>();
@@ -32,20 +36,28 @@ public class ViewExtractor {
             sourcePattern = joinFromAST(join, sourcePattern, namedRefs);
         }
         var query = new Query(new Selection(sourcePattern, Pattern.from(target)));
+
         // TODO: Filters???
+        if(targetClass.source().condition() != null) {
+            throw new RuntimeException("Condition expressions are not supported");
+        }
+
         applyProjectionsFromAST(targetClass.features(), query, namedRefs, target);
+
+        if (!targetClass.source().groupingExpressions().isEmpty()) {
+            throw new RuntimeException("Grouping expressions are not supported");
+        }
 
         return query;
     }
 
     private static Pattern joinFromAST(AQRJoin join, Pattern pattern, Map<String, FQN> namedRefs) {
         var fqn = fqn(join.from());
-        if (!join.from().alias().isEmpty()) {
-            namedRefs.put(join.from().alias(), fqn);
-        }
+        Optional<String> joinFromAlias = Optional.ofNullable(join.from().alias()).filter(alias -> !alias.isEmpty());
+        joinFromAlias.ifPresent(s -> namedRefs.put(s, fqn));
 
         if (join.type() == AQRJoin.Type.Inner) {
-            var attributes = join.featureConditions().stream().flatMap(feature -> feature.features().stream()).map(attr -> new Tuple<>(attr, attr)).toList();
+            var attributes = join.featureConditions().stream().map(AQRJoin.FeatureCondition::features).flatMap(List::stream).map(attr -> new Tuple<>(attr, attr)).toList();
             return pattern.join(fqn, attributes);
         }
         // TODO: Other join types
@@ -56,12 +68,12 @@ public class ViewExtractor {
     private static void applyProjectionsFromAST(List<AQRFeature> features, Query query, Map<String, FQN> namedRefs, FQN target) {
         for (AQRFeature feature : features) {
             // TODO: Expressions, calculations, references??
-            if (feature instanceof AQRFeature.Attribute) {
+            if (feature instanceof AQRFeature.Attribute && feature.kind() instanceof AQRFeature.Kind.Copy) {
                 query.project(feature.name());
             } else if (feature instanceof AQRFeature.Reference) {
-
+                // TODO: Reference operators
             } else {
-                throw new RuntimeException("Invalid feature: " + feature);
+                throw new RuntimeException("Invalid projection: " + feature);
             }
         }
     }
