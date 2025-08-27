9bb14975f8f088b3c4ac8611db855af091b286c2
nilslambertz <nilslambertz@web.de>
2025-08-27 20:34:41 +0200

    Store unsupported expression in Exception

--- lang/expression-parser/parser/src/main/java/tools/vitruv/neojoin/expression_parser/parser/exception/UnsupportedReferenceExpressionException.java
+++ lang/expression-parser/parser/src/main/java/tools/vitruv/neojoin/expression_parser/parser/exception/UnsupportedReferenceExpressionException.java
@@ -1,9 +1,15 @@
 package tools.vitruv.neojoin.expression_parser.parser.exception;
 
+import lombok.Value;
+
 import org.eclipse.xtext.xbase.XExpression;
 
+@Value
 public class UnsupportedReferenceExpressionException extends Exception {
-    public UnsupportedReferenceExpressionException(String message, XExpression expression) {
-        super(String.format("The expression %s is not supported: %s", expression, message));
+    XExpression expression;
+
+    public UnsupportedReferenceExpressionException(XExpression expression) {
+        super(String.format("The expression %s is not supported", expression));
+        this.expression = expression;
     }
 }
