bfacf2d0b3e06f2f2ddf1124c7de629a13d18191
nilslambertz <nilslambertz@web.de>
2025-08-29 23:44:49 +0200

    Add generator for eMSL metamodels

---
+++ lang/backend-tgg/emsl_metamodel_generator/src/main/java/tools/vitruv/neojoin/tgg/emsl_metamodel_generator/EmslMetamodelGenerator.java
@@ -0,0 +1,23 @@
+package tools.vitruv.neojoin.tgg.emsl_metamodel_generator;
+
+import gg.jte.ContentType;
+import gg.jte.TemplateEngine;
+import gg.jte.output.FileOutput;
+
+import org.eclipse.emf.ecore.resource.ResourceSet;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class EmslMetamodelGenerator {
+    private static final String EMSL_METAMODEL_TEMPLATE = "emsl-metamodel.jte";
+
+    public static void generateMetamodels(ResourceSet set, Path output) {
+        TemplateEngine engine = TemplateEngine.createPrecompiled(ContentType.Plain);
+        try (FileOutput out = new FileOutput(output)) {
+            engine.render(EMSL_METAMODEL_TEMPLATE, set, out);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
