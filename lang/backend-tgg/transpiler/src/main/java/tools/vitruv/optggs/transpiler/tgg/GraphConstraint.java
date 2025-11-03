d7a27c9fc229ec033c7de01924fd6a92d0b591f6
nilslambertz <nilslambertz@web.de>
2025-11-03 23:47:53 +0100

    Generate unique ID for each constraint

--- lang/backend-tgg/transpiler/src/main/java/tools/vitruv/optggs/transpiler/tgg/GraphConstraint.java
+++ lang/backend-tgg/transpiler/src/main/java/tools/vitruv/optggs/transpiler/tgg/GraphConstraint.java
@@ -3,8 +3,11 @@ package tools.vitruv.optggs.transpiler.tgg;
 import lombok.Value;
 
 import java.util.List;
+import java.util.UUID;
 
 @Value
 public class GraphConstraint {
+    UUID id = UUID.randomUUID();
+
     List<Node> nodes;
 }
