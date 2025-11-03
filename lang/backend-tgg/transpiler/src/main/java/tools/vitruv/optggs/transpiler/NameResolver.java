e52874afe99643a716d1ea6788e5c424c91d5156
nilslambertz <nilslambertz@web.de>
2025-11-03 23:48:06 +0100

    Generate unique name for each constraint and pattern

--- lang/backend-tgg/transpiler/src/main/java/tools/vitruv/optggs/transpiler/NameResolver.java
+++ lang/backend-tgg/transpiler/src/main/java/tools/vitruv/optggs/transpiler/NameResolver.java
@@ -1,8 +1,9 @@
 package tools.vitruv.optggs.transpiler;
 
 import tools.vitruv.optggs.operators.FQN;
-import tools.vitruv.optggs.transpiler.tgg.CorrespondenceType;
 import tools.vitruv.optggs.transpiler.tgg.Correspondence;
+import tools.vitruv.optggs.transpiler.tgg.CorrespondenceType;
+import tools.vitruv.optggs.transpiler.tgg.GraphConstraint;
 import tools.vitruv.optggs.transpiler.tgg.Node;
 import tools.vitruv.optggs.transpiler.tgg.TripleRule;
 
@@ -20,17 +21,35 @@ public interface NameResolver {
     default String resolveRuleName(TripleRule rule) {
         if (rule.isLinkRule()) {
             // Link rule
-            var originNodes = rule.allSourcesAsSlice().filterMapNodes(node -> !node.links().isEmpty(), node -> node.type().localName());
-            var destinationNodes = rule.allSourcesAsSlice().filterMapNodes(node -> node.links().isEmpty(), node -> node.type().localName());
-            return "Link" + String.join("And", originNodes) + "To" + String.join("And", destinationNodes);
+            var originNodes =
+                    rule.allSourcesAsSlice()
+                            .filterMapNodes(
+                                    node -> !node.links().isEmpty(),
+                                    node -> node.type().localName());
+            var destinationNodes =
+                    rule.allSourcesAsSlice()
+                            .filterMapNodes(
+                                    node -> node.links().isEmpty(),
+                                    node -> node.type().localName());
+            return "Link"
+                    + String.join("And", originNodes)
+                    + "To"
+                    + String.join("And", destinationNodes);
         } else {
             // Select rule
             var greenSources = rule.allSourcesAsSlice().findNodes(Node::isGreen);
             var greenTargets = rule.allTargetsAsSlice().findNodes(Node::isGreen);
             var sources = greenSources.stream().map(node -> node.type().localName()).toList();
             var targets = greenTargets.stream().map(node -> node.type().localName()).toList();
-            return "Select" + String.join("And", sources)
-                    + "As" + String.join("And", targets);
+            return "Select" + String.join("And", sources) + "As" + String.join("And", targets);
         }
+    }
+
+    default String resolvePatternName(GraphConstraint constraint) {
+        return "Constraint" + constraint.getId().toString().replace("-", "");
+    }
+
+    default String resolveConstraintName(GraphConstraint constraint) {
+        return "No" + resolvePatternName(constraint);
     }
 }
