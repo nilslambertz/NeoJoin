459f527def4fa14c13838a15b0637104854f18bd
nilslambertz <nilslambertz@web.de>
2025-11-10 18:57:03 +0100

    Convert to record

--- lang/backend-tgg/transpiler/src/main/java/tools/vitruv/optggs/transpiler/graph/Parameter.java
+++ lang/backend-tgg/transpiler/src/main/java/tools/vitruv/optggs/transpiler/graph/Parameter.java
@@ -2,23 +2,7 @@ package tools.vitruv.optggs.transpiler.graph;
 
 import tools.vitruv.optggs.operators.expressions.ValueExpression;
 
-public final class Parameter {
-    private final String attribute;
-    private ValueExpression value;
-
-    public Parameter(String attribute, ValueExpression value) {
-        this.attribute = attribute;
-        this.value = value;
-    }
-
-    public String attribute() {
-        return attribute;
-    }
-
-    public ValueExpression value() {
-        return value;
-    }
-
+public record Parameter(String attribute, ValueExpression value) {
     Parameter deepCopy() {
         return new Parameter(attribute, value.deepCopy());
     }
