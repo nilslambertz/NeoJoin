35d44e19a6fad1af11c5a7497ff49e3dba2f7d61
nilslambertz <nilslambertz@web.de>
2025-11-16 22:06:59 +0100

    Add parser for eMSL files, use dependencies from eMoflone p2 repository

---
+++ lang/emsl-parser/src/main/java/tools/vitruv/neojoin/emsl_parser/EmslParser.java
@@ -0,0 +1,50 @@
+package tools.vitruv.neojoin.emsl_parser;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.xtext.resource.IResourceServiceProvider;
+import org.eclipse.xtext.resource.XtextResource;
+import org.eclipse.xtext.util.CancelIndicator;
+import org.eclipse.xtext.validation.CheckMode;
+import org.eclipse.xtext.validation.Issue;
+import org.emoflon.neo.emsl.EMSLStandaloneSetup;
+
+import java.util.List;
+
+public class EmslParser {
+    static {
+        new EMSLStandaloneSetup().createInjectorAndDoEMFRegistration();
+    }
+
+    public static List<EObject> parse(String path) {
+        // load
+        var resourceServiceProvider =
+                IResourceServiceProvider.Registry.INSTANCE.getResourceServiceProvider(
+                        URI.createFileURI(path));
+        if (resourceServiceProvider == null) {
+            throw new IllegalStateException(
+                    "Could not find IResourceServiceProvider for URI: "
+                            + path
+                            + ". Make sure the file has the correct extension (.msl) "
+                            + "and the language is registered.");
+        }
+
+        var resourceSet = resourceServiceProvider.get(ResourceSet.class);
+        var resource = resourceSet.getResource(URI.createFileURI(path), true);
+
+        // validate
+        var validator =
+                ((XtextResource) resource).getResourceServiceProvider().getResourceValidator();
+        var issues = validator.validate(resource, CheckMode.ALL, CancelIndicator.NullImpl);
+        if (!issues.isEmpty()) {
+            throw new RuntimeException(generateIssuesMessage(issues));
+        }
+
+        return resource.getContents();
+    }
+
+    private static String generateIssuesMessage(List<Issue> issues) {
+        return String.join(System.lineSeparator(), issues.stream().map(Issue::getMessage).toList());
+    }
+}
