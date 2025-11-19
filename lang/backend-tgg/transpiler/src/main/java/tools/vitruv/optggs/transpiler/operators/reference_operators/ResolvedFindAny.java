20a1c340b19b189a495b7e76c7a99f3bbc96e7db
nilslambertz <nilslambertz@web.de>
2025-11-19 21:38:27 +0100

    Add correct constraint patterns for FindAny operator

--- lang/backend-tgg/transpiler/src/main/java/tools/vitruv/optggs/transpiler/operators/reference_operators/ResolvedFindAny.java
+++ lang/backend-tgg/transpiler/src/main/java/tools/vitruv/optggs/transpiler/operators/reference_operators/ResolvedFindAny.java
@@ -1,33 +1,52 @@
 package tools.vitruv.optggs.transpiler.operators.reference_operators;
 
-import tools.vitruv.optggs.operators.FQN;
 import tools.vitruv.optggs.transpiler.graph.pattern.GraphPattern;
 import tools.vitruv.optggs.transpiler.graph.pattern.PatternLink;
 import tools.vitruv.optggs.transpiler.graph.pattern.PatternNode;
 import tools.vitruv.optggs.transpiler.graph.tgg.GraphPathToNode;
+import tools.vitruv.optggs.transpiler.graph.tgg.TGGNode;
 import tools.vitruv.optggs.transpiler.graph.tgg.TripleRule;
 import tools.vitruv.optggs.transpiler.graph.tgg.TripleRulesBuilder;
+
+import java.util.List;
 
 public class ResolvedFindAny implements ResolvedReferenceOperator {
     @Override
     public void extendRules(TripleRulesBuilder builder) {
         final TripleRule latestRule = builder.getLatestRule();
 
-        final GraphPattern pattern = latestRule.convertSourceNodesToGraphPattern();
-
         final GraphPathToNode pathToLastNode = builder.getPathToLastNode();
-        final FQN lastNodeType = pattern.findNestedNode(pathToLastNode).getType();
+        final TGGNode nodeBeforeLastNode =
+                latestRule.findNestedSourceNode(pathToLastNode.pathToSecondLastNode());
+        final String lastLink = pathToLastNode.getLastLink();
 
-        // Duplicate last source node and incoming link
-        PatternNode duplicatedLastNode = pattern.addNode(lastNodeType);
-        PatternLink linkToLastDuplicatedLastNode =
-                new PatternLink(pathToLastNode.getLastLink(), duplicatedLastNode);
+        // Pattern with the last link duplicated (prevents two outgoing edges from the same node)
+        final GraphPattern patternWithSecondLinkFromSameRoot =
+                nodeBeforeLastNode.convertToPatternNode().toGraphPattern();
+        latestRule.convertSourceNodesToGraphPattern();
+        final PatternNode patternWithSecondLinkFromSameRootLastNode =
+                patternWithSecondLinkFromSameRoot.findNestedNode(
+                        new GraphPathToNode(nodeBeforeLastNode.getType(), List.of(lastLink)));
 
-        // Add the "duplicated" link to the node "above"
-        final PatternNode nodeBeforeLastNode =
-                pattern.findNestedNode(pathToLastNode.pathToSecondLastNode());
-        nodeBeforeLastNode.addLink(linkToLastDuplicatedLastNode);
+        final PatternNode patternWithSecondLinkFromSameRootCopiedLastNode =
+                patternWithSecondLinkFromSameRootLastNode.copyWithDifferentNames();
+        patternWithSecondLinkFromSameRoot.addNode(patternWithSecondLinkFromSameRootCopiedLastNode);
+        final PatternNode patternWithSecondLinkFromSameRootRootNode =
+                patternWithSecondLinkFromSameRoot
+                        .findNodeByType(nodeBeforeLastNode.getType())
+                        .orElseThrow();
+        patternWithSecondLinkFromSameRootRootNode.addLink(
+                new PatternLink(lastLink, patternWithSecondLinkFromSameRootCopiedLastNode));
 
-        builder.addConstraint(pattern);
+        // Pattern with two matching graphs (prevents two outgoing edges from different nodes);
+        final GraphPattern patternWithTwoSameGraphs =
+                nodeBeforeLastNode
+                        .convertToPatternNode()
+                        .toGraphPattern()
+                        .duplicateAllNodesWithDifferentNames();
+
+        // Add constraints to the grammar
+        builder.addConstraint(patternWithSecondLinkFromSameRoot);
+        builder.addConstraint(patternWithTwoSameGraphs);
     }
 }
